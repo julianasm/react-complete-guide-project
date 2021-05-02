1ba4d023a2ee6983ccd71009590a416c4e6a2c7b
Juliana <julianasouzamelo@live.com>
2021-05-02 10:26:48 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+/* root component, 
+it's the main component being rendered in index.js
+component tree
+
+*/
+export default App;
