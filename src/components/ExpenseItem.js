2758a051fd16546c97f02ebe45ae708481ceca24
Juliana <julianasouzamelo@live.com>
2021-05-02 11:34:20 -0300

    setting element props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
-function ExpenseItem() {
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+
+    // props tornam elementos reutilizaveis
     // regra: apenas um elemento 'root'(div) dentro do return
-    return <div>
-        <div>March 28th 2021</div>
-        <div>
-            <h2>Car Insurance</h2>
-            <div>$294.67</div>
+    return <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>
     ;
